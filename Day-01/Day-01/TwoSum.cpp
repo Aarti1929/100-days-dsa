4ab5e930c828834b28e828b1ed836ba6c1a79ce5
Aarti Thakur <ekaki43870@gmail.com>
2026-07-18 15:59:01 +0530

    Create TwoSum.cpp

---
+++ Day-01/Day-01/TwoSum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findDuplicate(vector<int>& nums) {
+
+    unordered_map<int, int> mpp;
+
+    for (int i = 0; i < nums.size(); i++) {
+        mpp[nums[i]]++;
+
+        if (mpp[nums[i]] > 1) {
+            return nums[i];
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 4, 2, 2};
+
+    cout << findDuplicate(nums);
+
+    return 0;
+}
